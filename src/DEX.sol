6a2ac2610805ff8cd43233fab3cf793deb4c3627
rkdnd <gaweon.kim@dream-academy.io>
2022-10-01 00:05:57 +0900

    DEX patch

--- src/DEX.sol
+++ src/DEX.sol
@@ -2,25 +2,20 @@
 pragma solidity ^0.8.13;
 
 import "./ERC20.sol";
-interface IDEX{
-    function swap(uint256 tokenXAmount, uint256 tokenYAmount, uint256 tokenMinimumOutputAmount) external returns (uint256 outputAmount);
-    function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPTokenAmount) external returns (uint256 LPTokenAmount);
-    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) external;
-}
 
-contract DEX is IDEX, ERC20{
-
+contract DEX is ERC20{
     address factory;
     address tokenX;
     address tokenY;
 
-    address[] LPowners;
-
     uint256 private reserve0;
     uint256 private reserve1;
 
-    constructor() ERC20("GWDEX", "GW") {
-        factory = msg.sender;
+    constructor(address _tokenX, address _tokenY) ERC20("GWDEX", "GW") {
+        require(_tokenX != _tokenY, "DA-DEX: Tokens should be different");
+
+        tokenX = _tokenX;
+        tokenY = _tokenY;
     }
 
     function init(address token0, address token1) external  {
@@ -35,90 +30,77 @@ contract DEX is IDEX, ERC20{
         _reserve1 = reserve1;
     }
 
-    function _update(uint balance0, uint balance1) private {
-        reserve0 = uint256(balance0);
-        reserve1 = uint256(balance1);
-    }
-
-    function calFee(uint256 inputAmount) internal returns(uint256 fee){
-        fee = inputAmount / 10;
+    function _update() private {
+        reserve0 = ERC20(tokenX).balanceOf(address(this));
+        reserve1 = ERC20(tokenY).balanceOf(address(this));
     }
 
     function swap(uint256 tokenXAmount, uint256 tokenYAmount, uint256 tokenMinimumOutputAmount) external returns (uint256 outputAmount){
-        require(tokenXAmount > 0 || tokenYAmount > 0, 'INSUFFICIENT_OUTPUT_AMOUNT');
         require(tokenXAmount == 0 || tokenYAmount == 0, 'INSUFFICIENT_OUTPUT_AMOUNT');
         
         (uint256 _reserve0, uint256 _reserve1) = getReserves(); //gas save
         (address inToken, address outToken, uint inputAmount, uint256 reserveIn, uint256 reserveOut) = 
             (tokenYAmount == 0) ? (tokenX, tokenY, tokenXAmount, _reserve0, _reserve1) : (tokenY, tokenX, tokenYAmount, _reserve1, _reserve0);
 
-        (reserveIn, reserveOut)  = _swap(inToken, outToken, inputAmount, reserveIn, reserveOut, tokenMinimumOutputAmount);
-        (tokenYAmount == 0) ? _update(reserveIn, reserveOut) : _update(reserveOut, reserveIn);
+        outputAmount = _swap(inToken, outToken, inputAmount, reserveIn, reserveOut, tokenMinimumOutputAmount);
+        _update();
     }
 
     function _swap(address inToken, address outToken, uint256 inputAmount, uint256 reserveIn, uint256 reserveOut, uint minimum)
-        internal returns (uint256 _reserveIn, uint256 _reserveOut){
-        uint256 fee = calFee(inputAmount);
-        inputAmount += fee;
-        uint256 balance = ERC20(inToken).balanceOf(msg.sender);
-        require(balance >= inputAmount, 'Over balance');
+        internal returns (uint256 outputAmount){
+        uint256 IntokenBalance = ERC20(inToken).balanceOf(msg.sender);
+        require(IntokenBalance >= inputAmount, 'balance exceeds');
 
         uint256 constK = reserveIn * reserveOut;
-        uint256 outputAmount = reserveOut - constK / (reserveIn + inputAmount);
+        uint256 calNum = constK / (reserveIn + inputAmount);
+        if(constK % (reserveIn + inputAmount) != 0)
+            calNum += 1;
+        outputAmount = (reserveOut - calNum) * 999 / 1000;
         require(outputAmount >= minimum, "lower than minimum output Tokens");
 
-        ERC20(inToken)._transfer(msg.sender ,address(this), inputAmount);
-        ERC20(outToken)._transfer(address(this), msg.sender, outputAmount);
-        feeProcedure(inToken, fee);
-        
-        _reserveIn = reserveIn + inputAmount;
-        _reserveOut = reserveOut - outputAmount;
-    }
-
-    function feeProcedure(address token, uint256 fee) internal {
-        for(uint i = 0 ; i < LPowners.length; i++){
-            uint earnedAmount = fee * _balances[LPowners[i]] / totalSupply();
-            ERC20(token)._transfer(address(this), LPowners[i], earnedAmount);
-        }
+        ERC20(inToken).transferFrom(msg.sender, address(this), inputAmount);
+        ERC20(outToken).transfer(msg.sender, outputAmount);
     }
 
     function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPTokenAmount)
     external returns (uint256 LPTokenAmount){
+        require(tokenXAmount!=0 && tokenYAmount !=0);
         (uint256 _reserve0, uint256 _reserve1) = getReserves();
-
         uint _totalSupply = totalSupply();
 
         if (_totalSupply == 0) {
             LPTokenAmount = sqrt(tokenXAmount * tokenYAmount);
-        //    _mint(address(0), minimumLPTokenAmount); // 풀 비우지 않기 위해 + zero division
+        //    _mint(address(0), 10000); // 풀 비우지 않기 위해 + zero division
         } else {
             LPTokenAmount = min((tokenXAmount * _totalSupply) / _reserve0, (tokenYAmount * _totalSupply) / _reserve1);
             // LPTokenAmount = min(sqrt((tokenXAmount * _totalSupply) / _reserve0), sqrt((tokenYAmount * _totalSupply) / _reserve1));
         }
 
         require(LPTokenAmount >= minimumLPTokenAmount);
+        ERC20(tokenX).transferFrom(msg.sender, address(this), tokenXAmount);
+        ERC20(tokenY).transferFrom(msg.sender, address(this), tokenYAmount);
         _mint(msg.sender, LPTokenAmount);
-        LPowners.push(msg.sender);
 
-        _update((tokenXAmount + _reserve0), (tokenYAmount + _reserve1));
+       _update();
     }
-    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) external{
+    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount)
+    external returns (uint256 transferX, uint256 transferY){
         uint liquidity = balanceOf(msg.sender);
         require(liquidity >= LPTokenAmount);
 
         (uint256 _reserve0, uint256 _reserve1) = getReserves();                            
         uint _totalSupply = totalSupply();
 
-        uint256 amount0 = LPTokenAmount * _reserve0 / _totalSupply;
-        uint256 amount1 = LPTokenAmount * _reserve1 / _totalSupply;
+        transferX = LPTokenAmount * _reserve0 / _totalSupply;
+        transferY = LPTokenAmount * _reserve1 / _totalSupply;
 
-        require(amount0 >= minimumTokenXAmount && amount1 >= minimumTokenYAmount);
-        _burn(address(this), LPTokenAmount);
+        require(transferX >= minimumTokenXAmount && transferY >= minimumTokenYAmount);
+        _burn(msg.sender, LPTokenAmount);
 
-        ERC20(tokenX)._transfer(address(this), msg.sender, amount0);
-        ERC20(tokenY)._transfer(address(this), msg.sender, amount1);
+        ERC20(tokenX).transfer(msg.sender, transferX);
+        ERC20(tokenY).transfer(msg.sender, transferY);
 
-        _update((_reserve0 - amount0), (_reserve1 - amount1));
+        _update();
     }
 
     function min(uint x, uint y) internal pure returns (uint z) {
@@ -136,17 +118,5 @@ contract DEX is IDEX, ERC20{
         } else if (y != 0) {
             z = 1;
         }
-    }
-
-    function add(uint x, uint y) internal pure returns (uint z) {
-        require((z = x + y) >= x, 'ds-math-add-overflow');
-    }
-
-    function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x, 'ds-math-sub-underflow');
-    }
-
-    function mul(uint x, uint y) internal pure returns (uint z) {
-        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
     }
 }
--- src/SwapFactory.sol
+++ src/SwapFactory.sol
@@ -1,30 +1,30 @@
-pragma solidity ^0.8.13;
+// pragma solidity ^0.8.13;
 
-import "./DEX.sol";
-contract SwapFactory{
-    mapping(address => mapping(address => address)) public getPool; //mapping 0로 초기화?
-    address[] public _pools;
+// import "./DEX.sol";
+// contract SwapFactory{
+//     mapping(address => mapping(address => address)) public tokenPair;
+//     address[] public _pools;
 
-    function createPool(address tokenX, address tokenY) public returns (address pAddress){
-        require(tokenX != tokenY);
-        (address token0, address token1) = tokenX < tokenY ? (tokenX, tokenY) : (tokenY, tokenX); 
-        require(token0 != address(0) || token1 != address(0)); //왜 token0 만 address(0) 검사?
-        require(getPool[token0][token1] == address(0), "pool exist");
+//     function createPool(address tokenX, address tokenY) public returns (address pAddress){
+//         require(tokenX != tokenY);
+//         (address token0, address token1) = tokenX < tokenY ? (tokenX, tokenY) : (tokenY, tokenX); 
+//         require(token0 != address(0) || token1 != address(0)); //왜 token0 만 address(0) 검사?
+//         require(tokenPair[token0][token1] == address(0), "pool exist");
 
-        DEX poolAddress = new DEX();
-        poolAddress.init(token0, token1);
-        pAddress = address(poolAddress);
+//         DEX poolAddress = new DEX();
+//         poolAddress.init(token0, token1);
+//         pAddress = address(poolAddress);
 
-        getPool[token0][token1] = pAddress;
-        getPool[token1][token0] = pAddress;
-        _pools.push(pAddress);
-    }
+//         tokenPair[token0][token1] = pAddress;
+//         tokenPair[token1][token0] = pAddress;
+//         _pools.push(pAddress);
+//     }
 
-    function getpool(address token0, address token1) public returns (address poolAddress){
-        if(getPool[token0][token1] == address(0)){
-            return createPool(token0, token1);
-        }else{
-            return getPool[token0][token1];
-        }
-    }
-}
+//     function getpool(address token0, address token1) public returns (address poolAddress){
+//         if(tokenPair[token0][token1] == address(0)){
+//             return createPool(token0, token1);
+//         }else{
+//             return tokenPair[token0][token1];
+//         }
+//     }
+// }
